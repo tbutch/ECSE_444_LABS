b00da3d078ec450bfc87773089e6dc432522afc0
Tristan Bouchard <tristan.bouchard@mail.mcgill.ca>
2018-10-15 18:19:44 -0400

    WIP Write values to analog IO using DAC

--- G14_Lab5/G14_Lab5/Src/stm32l4xx_it.c
+++ G14_Lab5/G14_Lab5/Src/stm32l4xx_it.c
@@ -36,7 +36,7 @@
 #include "stm32l4xx_it.h"
 
 /* USER CODE BEGIN 0 */
-
+extern int timerFlag;
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
@@ -219,7 +219,8 @@ void TIM2_IRQHandler(void)
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
-	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+	//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+	timerFlag = 1;
   /* USER CODE END TIM2_IRQn 1 */
 }
 
